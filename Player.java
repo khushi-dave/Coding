18464257671f68404d9b969d73f3d26b5081e6f5
khushi-dave <68141662+khushi-dave@users.noreply.github.com>
2020-10-21 00:02:27 +0530

    QuizApplication

---
+++ Player.java
@@ -0,0 +1,16 @@
+package com.quizapplication;
+import java.util.Scanner;
+
+public class Player {
+
+    Scanner sc=new Scanner(System.in);
+    String name;
+    int score=0;
+
+    public void getDetails(){
+
+        System.out.println("Enter player name:");
+        name=sc.next();
+
+    }
+}
